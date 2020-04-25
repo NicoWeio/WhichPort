1370a99c2daaf506673e006a43d383dba53457e2
Nicolai Weitkemper <nico.weio@gmail.com>
2020-04-25 22:07:17 +0200

    add types attribute

--- src/DATA.js
+++ src/DATA.js
@@ -2,10 +2,12 @@ export default [{
     name: 'ActivityWatch',
     icon: 'TODO',
     port: '5600',
+    types: ['app.ui', 'app.api'],
   },
   {
     name: 'Syncthing',
     icon: 'TODO',
     port: '8384',
+    types: ['app.ui'],
   },
 ];
